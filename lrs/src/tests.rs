f2bc4c0be06849cb8b2d563d6ecaf4ff9f59a24c
Katharina Fey <kookie@spacekookie.de>
2017-10-25 16:42:40 +0200

    Adding a remove unit test and now using emoji 🔥

--- lrs/src/tests.rs
+++ lrs/src/tests.rs
@@ -1,26 +1,49 @@
 //! Some unit tests for all the internal functionality of lrs
 
 #[cfg(test)]
+#[allow(dead_code)]
+#[allow(unused_macros)]
+#[allow(unused_imports)]
 mod tests {
     use terms::*;
     use equation::*;
 
-    macro_rules! symbol {
-        ($v:expr, $s:expr) => ( Symbol { val: $v, state: $s } );
-    }
+    // macro_rules! symbol {
+    //     ($v:expr, $s:expr) => ( Symbol { val: $v, state: $s } );
+    // }
 
     #[test]
      fn lrs_symbol_create() {
-        let s = symbol!('A', true);
+        let s = symbol!["A"];
         assert!(s.val == 'A');
         assert!(s.state == true);
     }
 
     #[test]
     fn lrs_term_create() {
-        let s = symbol!('A', true);
-        let t = Term::new(s);
+        let term = term!["A", "!🐎", "🐟", "C"];
 
-        assert!(t.contains(&symbol!('A', true)));
+        assert!(term.contains(&symbol!["A"]));
+        assert!(term.contains(&symbol!["!🐎"]));
+        assert!(term.contains(&symbol!["🐟"]));
+        assert!(term.contains(&symbol!["C"]));
+    }
+
+    #[test]
+    #[allow(unused_variables)]
+    fn lrs_term_remove() {
+        let mut term = term!["A", "!🐎", "🐟", "C"];
+
+        term.remove(symbol!["A"]);
+        assert!(! term.contains(&symbol!["A"]));
+
+        term.remove(symbol!["!🐎"]);
+        assert!(! term.contains(&symbol!["!🐎"]));
+        
+        term.remove(symbol!["🐟"]);
+        assert!(! term.contains(&symbol!["🐟"]));
+
+        term.remove(symbol!["C"]);
+        assert!(! term.contains(&symbol!["C"]));
     }
 }
